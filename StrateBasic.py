f68e2c53747c8a06d5ed293bf7d13a67e6c73bbd
iac26 <iacopo@citycable.ch>
2017-04-21 18:25:43 +0200

    Add files via upload

--- StrateBasic.py
+++ StrateBasic.py
@@ -3,6 +3,8 @@ import random
 
 norm = True
 
+
+#retourne un booleen selon des probabilités 
 def fight(p):
     r = random.randint(1,100)
     if p > r:
@@ -11,7 +13,7 @@ def fight(p):
         return False
     
 
-
+#permet de calculer le résultat d'un duel en se basant sur les probabilités données
 def duel(a,b):
     if a == 'F':
         if b == 'F':
@@ -35,12 +37,15 @@ def duel(a,b):
         if b == 'A':
             return fight(50)
 
-def count(armee, t):
+#compte le nombre de soldats d'un certain type dans une armée  
+def count(armee, t): 
     c = 0
     for s in armee:
         if s == t:
             c += 1
     return c
+
+#retourne le nom complet d'un type de soldat
 def tpe(t):
     if t == 'F':
         return 'Fantassin'
@@ -49,15 +54,16 @@ def tpe(t):
     if t == 'A':
         return 'Artilleur'
 
+
 resp = raw_input('Enter pour commencer...')
 if resp == 'special':
     norm = False
 while True:
-    if norm:
-        armeeRouge = ['F','F','F','F','F','F','F','F','F','F','F','C','C','C','C','C','A','A']
-        armeeBleue = ['F','F','F','F','F','F','F','F','F','F','F','C','C','C','C','C','A','A']
-    else:
-        armeeRouge = []
+    if norm: #configuration normale des armées
+        armeeRouge = ['F','F','F','F','F','F','F','F','F','F','F','C','C','C','C','C','A','A'] #contient les soldats vivants (Rouges) (dure toute la battaille)
+        armeeBleue = ['F','F','F','F','F','F','F','F','F','F','F','C','C','C','C','C','A','A'] #contient les soldats vivants (Bleus) (dure toute la battaille)
+    else: #configuration speciale (choix de l'utilisateur) des armées
+        armeeRouge = [] 
         armeeBleue = []
         print "\ncustomisation de l'armée rouge:\n"
         while True:
@@ -89,50 +95,56 @@ while True:
                 break
             except:
                 print 'ERREUR Nombres Entiers Requis !'
-    blessesRouges = []
-    blessesBleus = []
-    cadavresRouges = []
-    cadavresBleus = []
-    meleeRouge = []
-    meleeBleue = []
-    champ = 0
-    aff = 1
+    #initialisation des tableaux
+    blessesRouges = [] #contient les index des soldats tués (Rouges) (dure un affrontement)
+    blessesBleus = [] #contient les index des soldats tués (Bleus) (dure un affrontement)
+    cadavresRouges = [] #contient les soldats morts (Rouges) (dure toute la battaille)
+    cadavresBleus = [] #contient les soldats morts (Bleus) (dure toute la battaille)
+    meleeRouge = [] #contient les index des soldats vivants (Rouges) (dure un affrontement)
+    meleeBleue = []  #contient les index des soldats vivants (Bleus) (dure un affrontement)
+    champ = 0 # taille de l'armée la plus petite
+    aff = 1 # numero de l'affrontement
 
     print '\n\n\n\n\n\n------ Nouvelle Battaille ------'
     while True:
+        #arrangements des duels 
         meleeRouge = random.sample(range(len(armeeRouge)), len(armeeRouge))
         meleeBleue = random.sample(range(len(armeeBleue)), len(armeeBleue))
         blessesBleus = []
         blessesRouges = []
-        glandeurs = []
+        glandeurs = [] #contient les soldtas qui n'ont pas d'adversaire
+        #calcul de la taille de l'armée la plus petite et du non('ga') de l'armée la plus grande
         if len(meleeRouge) <= len(meleeBleue):
             champ = len(meleeRouge)
             ga = 'Bleue'
         else:
             champ = len(meleeBleue)
             ga = 'Rouge'
+        #affichage des effectifs
         print '\n------ Affrontement', aff, '------'
-        print "\nl'armée rouge est constituée de:"
-        print count(armeeRouge, 'F'), 'Fantassins', '(', count(cadavresRouges, 'F'), 'morts )'
-        print count(armeeRouge, 'C'), 'Cavaliers', '(', count(cadavresRouges, 'C'), 'morts )'
-        print count(armeeRouge, 'A'), 'Artilleurs', '(', count(cadavresRouges, 'A'), 'morts )'
-        print "\nl'armée bleue est constituée de:"
-        print count(armeeBleue, 'F'), 'Fantassins', '(', count(cadavresBleus, 'F'), 'morts )'
-        print count(armeeBleue, 'C'), 'Cavaliers', '(', count(cadavresBleus, 'C'), 'morts )'
-        print count(armeeBleue, 'A'), 'Artilleurs', '(', count(cadavresBleus, 'A'), 'morts )'
-        waste = raw_input("\ncommencer l'affrontement (Enter) ?")
+        print "\nl'armée rouge est constituée de", len(armeeRouge), 'soldat:' if len(armeeRouge) == 1 else 'soldats:'
+        print count(armeeRouge, 'F'), 'Fantassin' if count(armeeRouge, 'F') == 1 else 'Fantassins' , '(', count(cadavresRouges, 'F'), 'mort )' if count(cadavresRouges, 'F') == 1 else 'morts )'
+        print count(armeeRouge, 'C'), 'Cavalier' if count(armeeRouge, 'C') == 1 else 'Cavaliers' , '(', count(cadavresRouges, 'C'), 'mort )' if count(cadavresRouges, 'C') == 1 else 'morts )'
+        print count(armeeRouge, 'A'), 'Artilleur' if count(armeeRouge, 'A') == 1 else 'Artilleurs' , '(', count(cadavresRouges, 'A'), 'mort )' if count(cadavresRouges, 'A') == 1 else 'morts )'
+        print "\nl'armée bleue est constituée de", len(armeeBleue), 'soldat:' if len(armeeBleue) == 1 else 'soldats:'
+        print count(armeeBleue, 'F'), 'Fantassin' if count(armeeBleue, 'F') == 1 else 'Fantassins' , '(', count(cadavresBleus, 'F'), 'mort )' if count(cadavresBleus, 'F') == 1 else 'morts )'
+        print count(armeeBleue, 'C'), 'Cavalier' if count(armeeBleue, 'C') == 1 else 'Cavaliers' , '(', count(cadavresBleus, 'C'), 'mort )' if count(cadavresBleus, 'C') == 1 else 'morts )'
+        print count(armeeBleue, 'A'), 'Artilleur' if count(armeeBleue, 'A') == 1 else 'Artilleurs' , '(', count(cadavresBleus, 'A'), 'mort )' if count(cadavresBleus, 'A') == 1 else 'morts )'
+        waste = raw_input("\ncommencer l'affrontement (Enter) ?") 
         ct = 0
+        #calcul des duels et des gagnants + affichage
         for d in range(champ):
             ct += 1
             print '\nun', tpe(armeeRouge[meleeRouge[d]]), 'rouge se bat contre un', tpe(armeeBleue[meleeBleue[d]]), 'bleu:',
             if duel(armeeRouge[meleeRouge[d]], armeeBleue[meleeBleue[d]]):
                 cadavresBleus.append(armeeBleue[meleeBleue[d]])
                 blessesBleus.append(meleeBleue[d])
-                print 'le', tpe(armeeRouge[meleeRouge[d]]), 'rouge gangne!'
+                print 'l\'' if armeeRouge[meleeRouge[d]] == 'A' else 'le', tpe(armeeRouge[meleeRouge[d]]), 'rouge gangne!'
             else:
                 cadavresRouges.append(armeeRouge[meleeRouge[d]])
                 blessesRouges.append(meleeRouge[d])
-                print 'le', tpe(armeeBleue[meleeBleue[d]]), 'bleu gangne!'
+                print 'l\'' if armeeBleue[meleeBleue[d]] == 'A' else 'le', tpe(armeeBleue[meleeBleue[d]]), 'bleu gangne!'
+        #calcul des soldats sans adversaire + affichage
         if ga == 'Rouge':        
             for g in range(ct, len(meleeRouge)):
                 print '\nun', tpe(armeeRouge[meleeRouge[g]]), 'rouge ne se bat pas'
@@ -141,7 +153,7 @@ while True:
             for g in range(ct, len(meleeBleue)):
                 print '\nun', tpe(armeeBleue[meleeBleue[g]]), 'bleu ne se bat pas'
                 glandeurs.append(armeeBleue[meleeBleue[g]])
-
+        #répartition des soldats dans le champ de battaille 
         while True:
             ctg = 0
             grid = [' ' for i in range(36)]
@@ -205,10 +217,12 @@ while True:
                         break
             if ok:
                 break
+        #affichage du champ de battaille 
         ct1 = 0
         ct2 = 0
         ct3 = 0
-        st = '\n'
+        st = ' '
+        st += '\n'
         for a in range(6):
             for b in range(6):
                 if grid[ct1] == ' ' and ct3 < len(glandeurs):
@@ -222,27 +236,29 @@ while True:
                 st += comb1[ct2] + '   '
                 ct2 += 1
             st += '\n'
-
+        st += ' '
         print st
+        #suppression des morts des tableaux armee...
         blessesBleus.sort(reverse=True)
         blessesRouges.sort(reverse=True)
         for b in blessesBleus:
             armeeBleue.pop(b)
         for r in blessesRouges:
             armeeRouge.pop(r)
+        #conditions de victoire + affichage des effectifs de l'armée vaincqueure 
         if len(armeeRouge) == 0:
             print "\n\n\nl'armée bleue gagne!"
-            print "\nelle est encore constituée de:\n"
-            print count(armeeBleue, 'F'), 'Fantassins', '(', count(cadavresBleus, 'F'), 'morts )'
-            print count(armeeBleue, 'C'), 'Cavaliers', '(', count(cadavresBleus, 'C'), 'morts )'
-            print count(armeeBleue, 'A'), 'Artilleurs', '(', count(cadavresBleus, 'A'), 'morts )'
+            print "\nelle est encore constituée de", len(armeeBleue), 'soldat:' if len(armeeBleue) == 1 else 'soldats:'
+            print count(armeeBleue, 'F'), 'Fantassin' if count(armeeBleue, 'F') == 1 else 'Fantassins' , '(', count(cadavresBleus, 'F'), 'mort )' if count(cadavresBleus, 'F') == 1 else 'morts )'
+            print count(armeeBleue, 'C'), 'Cavalier' if count(armeeBleue, 'C') == 1 else 'Cavaliers' , '(', count(cadavresBleus, 'C'), 'mort )' if count(cadavresBleus, 'C') == 1 else 'morts )'
+            print count(armeeBleue, 'A'), 'Artilleur' if count(armeeBleue, 'A') == 1 else 'Artilleurs' , '(', count(cadavresBleus, 'A'), 'mort )' if count(cadavresBleus, 'A') == 1 else 'morts )'
             break
         if len(armeeBleue) == 0:
             print "\nl'armée rouge gagne!"
-            print "\nelle est encore constituée de:\n"
-            print count(armeeRouge, 'F'), 'Fantassins', '(', count(cadavresRouges, 'F'), 'morts )'
-            print count(armeeRouge, 'C'), 'Cavaliers', '(', count(cadavresRouges, 'C'), 'morts )'
-            print count(armeeRouge, 'A'), 'Artilleurs', '(', count(cadavresRouges, 'A'), 'morts )'
+            print "\nelle est encore constituée de", len(armeeRouge), 'soldat:' if len(armeeRouge) == 1 else 'soldats:'
+            print count(armeeRouge, 'F'), 'Fantassin' if count(armeeRouge, 'F') == 1 else 'Fantassins' , '(', count(cadavresRouges, 'F'), 'mort )' if count(cadavresRouges, 'F') == 1 else 'morts )'
+            print count(armeeRouge, 'C'), 'Cavalier' if count(armeeRouge, 'C') == 1 else 'Cavaliers' , '(', count(cadavresRouges, 'C'), 'mort )' if count(cadavresRouges, 'C') == 1 else 'morts )'
+            print count(armeeRouge, 'A'), 'Artilleur' if count(armeeRouge, 'A') == 1 else 'Artilleurs' , '(', count(cadavresRouges, 'A'), 'mort )' if count(cadavresRouges, 'A') == 1 else 'morts )'
             break
         aff += 1
         waste = raw_input("\nvoir le prochain affrontement (Enter) ?")
