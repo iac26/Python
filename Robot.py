401b602b9f73abe7f6bbc9ca6520c88e2aa44f61
iac26 <iacopo@citycable.ch>
2017-05-29 10:51:27 +0200

    Add files via upload

--- Robot.py
+++ Robot.py
@@ -7,8 +7,9 @@ import cv2
 import time
 import os
 
+
 os.system('asebamedulla "ser:name=Thymio-II" ')
-time.sleep(1)
+time.sleep(2)
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SessionBus()
@@ -35,184 +36,209 @@ def proxH():
 
 def proxV():
     prox = []
-    for p in network.GetVariable("thymio-II", "prox.ground.ambiant")[0:2]:
+    for p in network.GetVariable("thymio-II", "prox.ground.delta")[0:2]:
         prox.append(p.real)
     return prox
 
-def avancer():
-    motorLeft(300)
-    motorRight(300)
+##def avancer():
+##    motorLeft(300)
+##    motorRight(300)
+##
+##def reculer():
+##    motorLeft(-300)
+##    motorRight(-300)
+##    
+##def stop():
+##    motorLeft(0)
+##    motorRight(0)
+##
+##def circleL():
+##    motorRight(100)
+##    motorLeft(-100)
+##
+##def circleR():
+##    motorRight(-100)
+##    motorLeft(100)
 
-def reculer():
-    motorLeft(-300)
-    motorRight(-300)
-    
-def stop():
-    motorLeft(0)
-    motorRight(0)
+##def explore():
+##    while True:
+##        p = []
+##        p = proxH()
+##        WL = [0.02, 0.01, 0, -0.009, -0.017]
+##        WR = [-0.017, -0.009, 0, 0.01, 0.02]
+##        L = 150
+##        R = 150
+##        for a in range(5):
+##            L += p[a]*WL[a]
+##            R += p[a]*WR[a]
+##        motorLeft(L)
+##        motorRight(R)
+##        time.sleep(0.1)
+##        
+##def BasicControl():
+##    if key == ord('w'):
+##        avancer()
+##    if key == ord('a'):
+##        circleL()
+##    if key == ord('d'):
+##        circleR()
+##    if key == ord('s'):
+##        reculer()
+##    if key == ord('x'):
+##        stop()
 
-def circleL():
-    motorRight(100)
-    motorLeft(-100)
 
-def circleR():
-    motorRight(-100)
-    motorLeft(100)
 
-def explore():
-    while True:
-        p = []
-        p = proxH()
-        WL = [0.02, 0.01, 0, -0.009, -0.017]
-        WR = [-0.017, -0.009, 0, 0.01, 0.02]
-        L = 150
-        R = 150
-        for a in range(5):
-            L += p[a]*WL[a]
-            R += p[a]*WR[a]
-        motorLeft(L)
-        motorRight(R)
-        time.sleep(0.1)
         
-def BasicControl():
-    if key == ord('w'):
-        avancer()
-    if key == ord('a'):
-        circleL()
-    if key == ord('d'):
-        circleR()
-    if key == ord('s'):
-        reculer()
-    if key == ord('x'):
-        stop()
 
-
-
+    
     
 
 
-#CAMERA STUFF
+#INITIALISATION DE LA CAMERA
 camera = PiCamera()
 camera.resolution = (368,240)
 camera.framerate = 24
 camera.hflip = True
 camera.vflip = True
 rawCapture = PiRGBArray(camera)
-#CAMERA HEATING
+#ON LAISSE LE TEMPS A LA CAMERA DE CHAUFFER
 time.sleep(0.1)
+
 
 #VARIABLES 
 #TRIANGLE MASK
 triangle_mask = np.zeros((240,368), np.uint8)
 triangle = np.array([[0,240],[184,120],[368,240]])
-#triangle = triangle.reshape((-1,1,2))
 cv2.fillPoly(triangle_mask,[triangle],True,255)
-#LEFT MASK
+#UPPER LEFT MASK
 upper_left_mask = np.zeros((240,368), np.uint8)
 upper_left_vertices = np.array([[0,240],[184,120],[184,180],[92,240]])
 cv2.fillPoly(upper_left_mask,[upper_left_vertices],True,255)
-#RIGHT MASK
+#UPPER RIGHT MASK
 upper_right_mask = np.zeros((240,368), np.uint8)
 upper_right_vertices = np.array([[368,240],[276,240],[184,180],[184,120]])
 cv2.fillPoly(upper_right_mask,[upper_right_vertices],True,255)
-#ClOSE LEFT MASK
+#LOWER LEFT MASK
 lower_left_mask = np.zeros((240,368), np.uint8)
 lower_left_vertices = np.array([[184,240],[184,180],[184,240]])
 cv2.fillPoly(lower_left_mask,[lower_left_vertices],True,255)
-#ClOSE RIGHT MASK
+#LOWER RIGHT MASK
 lower_right_mask = np.zeros((240,368), np.uint8)
 lower_right_vertices = np.array([[276,240],[184,180],[184,240]])
 cv2.fillPoly(lower_right_mask,[lower_right_vertices],True,255)
 
-
-#speed
+#VITESSE
 LSpeed = 0
 RSpeed = 0
 DSpeed = 100
 auto_steering = False
-#menu
+#MENU
 show_menu = False
 menu_cursor = 0
 menu_back = np.zeros((240,368,3), np.uint8)
 menu_back.fill(50)
-menu_back = cv2.rectangle(menu_back, (20,20), (348,220), (255,0,0), -1)
-menu = [['show speed', 1,40,1,0,1,1],['show guide', 1,60,1,0,1,1], ['show edges', 1,80,1,0,1,1],['show fps', 1,100,1,0,1,1],['blur', 7,120,1,1,9,2]]
-#show_speed = menu[0][1]
-#show_guide = menu[1][1]
-
-
-#emergency
+menu_back = cv2.rectangle(menu_back, (20,20), (348,220), (255,50,50), -1)
+menu_back = cv2.rectangle(menu_back, (20,20), (348,40), (255, 0,0), -1)
+menu = [['show speed', 1,40,1,0,1,1],['show guide', 1,60,1,0,1,1], ['show edges', 1,80,1,0,1,1],['show fps', 1,100,1,0,1,1],['blur', 7,120,1,1,9,2], ['clear screen', 0,140,1,0,1,1]]
+#QUITTER
+quit_confirm_back = np.zeros((240,368,3), np.uint8)
+quit_confirm_back.fill(50)
+quit_confirm_back = cv2.rectangle(quit_confirm_back, (60,60), (308,180), (255,50,50), -1)
+quit_confirm = False
+qStart = 0
+#EMERGENCE
 front_emergency = False
 back_emergency = False
 ground_emergency = False
 emergency = False
-interrupt = True
-timer = 20
+#MARCHE ARRIERE
+reverse = False
+left_reverse = False
+right_reverse = False
+back_reverse = False
+rStart = 0
+#IMAGES PAR SECONDE
 FTime = 0
 FPS = 0
+    
+    
 
-
+#ITERATION PRINCIPALE
 for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
+    #debut de l iteration
     STime = time.time()
     image = frame.array
     
-    #average command speed
+    #vitesse (ordonnee par l utilisateur)
     MSpeed = (RSpeed + LSpeed)/2
-    #true speed
+    #vraie vitesse 
     TSpeed = (speedLeft() + speedRight())/2
-    #timer
-    if timer == 0:
-        interrupt = False
-    else:
-        timer -= 1
     
     
-    #PROCESSING
+    #filtre de detection de bords
     blur = menu[4][1]
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (blur,blur), 1)
     canny = cv2.Canny(gray, 200, 20)
     processed = canny.copy()
 
-    #menu
     
 
-    #GUI writing
-    if menu[2][1]:
-        edges = cv2.cvtColor(processed, cv2.COLOR_GRAY2RGB)
-        edges[:,:,0]= 0
-        edges[:,:,2]= 0
-        cv2.addWeighted(edges, 0.2, image, 0.8, 0.0, image)
-        
-    if menu[0][1]:
-        cv2.putText(image, 'Speed: '+str(TSpeed),(0,15), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2)
-    if menu[3][1]:
-        cv2.putText(image, str(FPS),(340,15), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2)
-    if menu[1][1]:
-        cv2.polylines(image,[upper_left_vertices],True,(0,255,0))
-        cv2.polylines(image,[upper_right_vertices],True,(0,255,0))
-        cv2.polylines(image,[lower_left_vertices],True,(0,255,0))
-        cv2.polylines(image,[lower_right_vertices],True,(0,255,0))
+    #interface utilisateur
+    if not menu[5][1]:#clear screen
+        if menu[2][1]:#show edges
+            edges = cv2.cvtColor(processed, cv2.COLOR_GRAY2RGB)
+            edges[:,:,0]= 0
+            edges[:,:,2]= 0
+            cv2.addWeighted(edges, 0.2, image, 0.8, 0.0, image)
+            
+        if menu[0][1]:#show speed
+            cv2.putText(image, 'Speed: '+str(TSpeed),(0,15), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2)
+        if menu[3][1]:#show fps
+            cv2.putText(image, str(FPS),(340,15), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2)
+        if menu[1][1]:#show guide
+            cv2.polylines(image,[upper_left_vertices],True,(0,255,0))
+            cv2.polylines(image,[upper_right_vertices],True,(0,255,0))
+            cv2.polylines(image,[lower_left_vertices],True,(0,255,0))
+            cv2.polylines(image,[lower_right_vertices],True,(0,255,0))
+        if auto_steering:#autopilote
+            cv2.putText(image, 'AUTOSTEERING',(130,15), cv2.FONT_HERSHEY_PLAIN,1, (255,0,0),2)
+        if emergency:#emergence
+            cv2.putText(image, 'EMERGENCY',(140,15), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255),2)
+        if front_emergency:
+            cv2.putText(image, 'FRONT',(160,30), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255),2)
+        elif back_emergency:
+            cv2.putText(image, 'BACK',(165,30), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255),2)
+        elif ground_emergency:
+            cv2.putText(image, 'GROUND',(155,30), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255),2)
+
         
     
-    cv2.polylines(processed,[triangle],True,(255,255,255))
+    #cv2.polylines(processed,[triangle],True,(255,255,255))
 
     #menu
     if show_menu:
         for m in menu:
             m[3] = 1
-            menu[menu_cursor][3] = 2
+            menu[menu_cursor][3] = -1
         cv2.addWeighted(menu_back, 0.5, image, 0.5, 0, image)
-        cv2.putText(image, 'MENU', (165, 35), cv2.FONT_HERSHEY_PLAIN, 1, (20,0,0), 2)
-        cv2.rectangle(image,(20,20),(348,40),(225,0,0))
+        cv2.putText(image, 'MENU', (165, 35), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
         for m in menu:
-            cv2.rectangle(image,(20,m[2]),(348,m[2]+20),(200,0,0), m[3])
-            cv2.putText(image, m[0], (25, m[2]+15), cv2.FONT_HERSHEY_PLAIN, 1, (20,0,0), 2)
-            cv2.putText(image, str(m[1]), (300, m[2]+15), cv2.FONT_HERSHEY_PLAIN, 1, (20,0,0), 2)
-        cv2.rectangle(image,(20,20),(348,220),(225,0,0),2)
+            cv2.rectangle(image,(20,m[2]),(348,m[2]+20),(200,50,50), m[3])
+            cv2.putText(image, m[0], (25, m[2]+15), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+            cv2.putText(image, str(m[1]), (300, m[2]+15), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        cv2.rectangle(image,(20,20),(348,220),(200,50,50),1)
+    #quit    
+    if quit_confirm:
+        cv2.addWeighted(quit_confirm_back, 0.5, image, 0.5, 0, image)
+        cv2.putText(image, 'QUIT?', (165, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        #cv2.putText(image, 'Q:YES', (110, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        #cv2.putText(image, 'X:NO', (220, 150), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        cv2.rectangle(image,(60,60),(308,180),(0,0,0),1)
+    if time.time() - qStart > 3:
+        quit_confirm = False
         
-    if emergency:
-        cv2.putText(image, 'EMERGENCY',(140,15), cv2.FONT_HERSHEY_PLAIN,1, (0,0,255),2)
     
 
     
@@ -241,62 +267,123 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
 
     #IR SAFETY
     prox = proxH()
-    proxF = prox[:4]
+    proxv = proxV()
+    proxF = prox[:5]
     proxB = prox[5:7]
     if max(proxF) > 3000:
         front_emergency = True
         emergency = True
-        auto_steering = False
+        #auto_steering = False
     if max(proxB) > 3000:
         back_emergency = True
+        emergency = True
+        #auto_steering = False
+    if min(proxv) < 100:
+        ground_emergency = True
         emergency = True
         auto_steering = False
 
     
     #AUTOSTEERING
     if auto_steering:
-        cv2.putText(image, 'AUTOSTEERING',(130,15), cv2.FONT_HERSHEY_PLAIN,1, (255,0,0),2)
-        if interrupt:
-            if lower_rightN < lower_leftN:
-                RSpeed = -50
-                LSpeed = 50
-                timer = 20
-            else:
-                RSpeed = 50
-                LSpeed = -50
-                timer = 20
+        if reverse: #marche arriere
+            if right_reverse:
+                if (time.time() - rStart) < 3 :
+                    LSpeed = -50
+                    RSpeed = -50
+                elif (time.time() - rStart) < 7 :
+                    LSpeed = -100
+                    RSpeed = -50
+                else:
+                    reverse = False
+                    right_reverse = False
+                    emergency = False
+                    front_emergency = False
+                    
+            elif left_reverse:
+                if (time.time() - rStart) < 3 :
+                    LSpeed = -50
+                    RSpeed = -50
+                elif (time.time() - rStart) < 7 :
+                    LSpeed = -50
+                    RSpeed = -100
+                else:      
+                    rStart = 0
+                    reverse = False
+                    right_reverse = False
+                    emergency = False
+                    front_emergency = False
+##            elif back_reverse:
+##                if (time.time() - rStart) < 5 :
+##                    LSpeed = -50
+##                    RSpeed = -50
+##                elif (time.time() - rStart) < 10 :
+##                    ground_emergency = False
+##                    LSpeed = -50
+##                    RSpeed = 50
+##                else:      
+##                    rStart = 0
+##                    reverse = False
+##                    back_reverse = False
+##                    emergency = False
+                    
+            
 
-        else:    
+        else:
+            #OBSTACLE LOIN
             if upper_leftN < upper_rightN:
                 direction = 'left'
                 LSpeed = DSpeed - 50
                 RSpeed = DSpeed + 50
+            #OBSTACLE LOIN
             elif upper_leftN > upper_rightN:
                 direction = 'right'
                 LSpeed = DSpeed + 50
                 RSpeed = DSpeed - 50
-            #elif lower_leftN > 10 or lower_rightN > 10:
-            #    interrupt = True
-                
+            #OBSTACLE PROCHE
+            elif lower_leftN > 5 or lower_rightN > 5:
+                if lower_rightN < lower_leftN:
+                    RSpeed = -100
+                    LSpeed = 100
+                else:
+                    RSpeed = 100
+                    LSpeed = -100
+            #CONTACT
+            elif front_emergency:
+                rStart = time.time()
+                reverse = True
+                if proxF[0]+proxF[1] < prox[3]+proxF[4]:
+                    right_reverse = True
+                else:
+                    left_reverse = True
+##            elif ground_emergency:
+##                rStart = time.time()
+##                reverse = True
+##                back_reverse = True
             else:
                 direction = 'straight'
                 LSpeed = DSpeed
                 RSpeed = DSpeed
-            #cv2.putText(image, direction ,(290,30), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2) 
+                #cv2.putText(image, direction ,(290,30), cv2.FONT_HERSHEY_PLAIN,1, (0,255,0),2) 
     
     
 
 
     #GUI
     cv2.imshow('iTExplorer', image)
+    #cv2.imwrite('stream.jpeg', image)
     key = cv2.waitKey(1) & 0xFF
 
+
+    
+    #debut de la nouvelle image
     rawCapture.truncate(0)
+    
 
 
     #CONTROLS
 
-    if show_menu:
+    if show_menu: #controles du menu
         if key == ord('s'):
             menu_cursor += 1
             if menu_cursor > len(menu)-1:
@@ -316,7 +403,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
                 
     
     else:
-        if auto_steering:
+        if auto_steering:#controles de l autopilote
             if key == ord('w'):
                 DSpeed += 50
                 LSpeed = DSpeed
@@ -325,10 +412,14 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
                 DSpeed -= 50
                 LSpeed = DSpeed
                 RSpeed = DSpeed      
-        else:
+        else:#controles manuels
             if key == ord('w'):
-                LSpeed += 100
-                RSpeed += 100
+                if not front_emergency and not ground_emergency:
+                    LSpeed += 100
+                    RSpeed += 100
+                else:
+                    LSpeed = 0
+                    RSpeed = 0
             if key == ord('a'):
                 LSpeed -= 50
                 RSpeed += 50
@@ -336,30 +427,37 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
                 LSpeed += 50
                 RSpeed -= 50
             if key == ord('s'):
-                LSpeed -= 100
-                RSpeed -= 100
+                if not back_emergency:
+                    LSpeed -= 100
+                    RSpeed -= 100
+                else:
+                    LSpeed = 0
+                    RSpeed = 0
 
     
 
-    if key == ord('q'):
-        break
-    if key == ord('x'):
+    if key == ord('q'): #quitter
+        if quit_confirm:
+            break
+        qStart = time.time()
+        quit_confirm = True
+    if key == ord('x'): # arreter les moteurs
         LSpeed = 0
         RSpeed = 0
         auto_steering = False
         
-    if key == ord('y'):
+    if key == ord('y'): # activer/desactiver l autopilote
         if auto_steering:
             auto_steering = False
         else:
             auto_steering = True
-    if key == ord('m'):
+    if key == ord('m'): # ouvrir/fermer le menu
         if show_menu:
             show_menu = False
         else:
             show_menu = True
             
-    if key == ord('e'):
+    if key == ord('e'): # arret d urgence
         if emergency:
             emergency = False
             front_emergency = False
@@ -371,7 +469,7 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
             RSpeed = 0
             auto_steering = False
 
-    #Emergency "breaks"
+    #freins d emergence
     if front_emergency:
         if MSpeed > 0:
             LSpeed = 0
@@ -380,15 +478,22 @@ for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=
         if MSpeed < 0:
             LSpeed = 0
             RSpeed = 0
+    if ground_emergency:
+        if MSpeed > 0:
+            LSpeed = 0
+            RSpeed = 0
         
     
             
-    motorLeft(LSpeed)
+    motorLeft(LSpeed) 
     motorRight(RSpeed)
-    FTime = time.time() - STime
+    # calcul du nombre d images par sec
+    FTime = time.time() - STime 
     FPS = int(1 / FTime)
 
-stop()        
+
+motorLeft(0)
+motorRight(0)       
 cv2.destroyAllWindows()
 
     
