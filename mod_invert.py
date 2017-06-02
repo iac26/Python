6d1ed3ad73629f7e6ca5f76e3119ff911c76ef86
iac26 <iacopo@citycable.ch>
2017-06-02 18:50:32 +0200

    Add files via upload

--- mod_invert.py
+++ mod_invert.py
@@ -1,3 +1,5 @@
+import math
+
 def invert(e, mod):
     a = mod
     b = e % mod
@@ -10,4 +12,30 @@ def invert(e, mod):
     d = t
     if d < 0:
         d += mod
-    return d 
+    return d
+
+def exp_mod(num, e, mod):
+    R = []
+    rp = num
+    p = e
+    r = 0
+    while p >= 1:
+        r = (rp ** 2) % mod
+        if p % 2 == 1:
+            R.append(rp)
+            
+        p, rp = p / 2, r
+    s = 1
+    for r in R:
+        s *= r
+    return s % mod
+
+def guess(n):
+    for i in range(2,int(math.sqrt(n))):
+        if n % i == 0:
+            p = n/i
+            q = i
+            return [p,q]
+                   
+                   
+            
